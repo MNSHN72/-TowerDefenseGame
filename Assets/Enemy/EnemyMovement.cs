3022b9d62b18c4d01cc071d535e54ce9698313bb
MNSHN72 <gigamilk69@gmail.com>
2021-05-14 03:03:04 +0900

    -TowerDefense- 2021/05/14  3:02
    #time to learn about coroutines
    #so coroutines are kinda complicated and confusing
    #but pretty cool :)
    #It seems like alot of backend stuff I quite dont understand
    # but I think i get the gist of it
    #KEYWORDS:
    # Ienumerable
    #startcoroutine stopcoroutine
    #yield return
    #yieldinstructions

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] private float waitTime = 1f;
     // Start is called before the first frame update
     private void Start()
     {
-        PrintWaypointNames();
+        StartCoroutine(FollowPath());
     }
 
-    private void PrintWaypointNames()
+    private IEnumerator FollowPath()
     {
         foreach (Waypoint wp in path)
         {
-            Debug.Log(wp.name);
+            this.gameObject.transform.position = wp.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
