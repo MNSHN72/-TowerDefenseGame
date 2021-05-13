ee12c1bba2fe50d796617441189140f736a434a8
MNSHN72 <gigamilk69@gmail.com>
2021-05-14 08:12:22 +0900

    -TowerDefense- 2021/05/14  8:11
    #added turtle model to enemy prefab
    #Implimented Lerp

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] private float waitTime = 1f;
+    [SerializeField] [Range(0f,5f)]private float speed = 1f;
     // Start is called before the first frame update
     private void Start()
     {
@@ -17,8 +17,18 @@ public class EnemyMovement : MonoBehaviour
     {
         foreach (Waypoint wp in path)
         {
-            this.gameObject.transform.position = wp.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = this.gameObject.transform.position;
+            Vector3 endPosition    = wp.transform.position;
+            float travelPercent = 0f;
+
+            this.gameObject.transform.LookAt(endPosition);
+
+            while (travelPercent<1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                this.gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
